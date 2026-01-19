549c6ae76bb09a14ebd44b4fbb1b1220b23d13b3
dimejiwebtech <dimejiweb@gmail.com>
2026-01-19 18:03:36 +0100

    minor changes

--- naija_recipes/settings.py
+++ naija_recipes/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=False, cast=bool)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
